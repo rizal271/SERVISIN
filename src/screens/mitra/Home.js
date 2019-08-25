0ba19640b1ef425f5105932074d123f41a5bffa7
rizal271 <rizalrohman23@gmail.com>
2019-08-25 20:12:54 +0700

    root navigation

---
+++ src/screens/mitra/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+class HomeMitra extends Component {
+    render() {
+        return (
+            <View>
+                <Text> textInComponent </Text>
+            </View>
+        )
+    }
+}
+export default HomeMitra
