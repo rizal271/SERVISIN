1421ca42b7b809cb9dfe300f57744754744625d8
falihnaufal17 <dudu.kotkot@gmail.com>
2019-08-25 23:33:02 +0700

    welcome screen with slider intro

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -1,1 +1,97 @@
+import React, { Component } from 'react'
+import { Text, View, Platform, StyleSheet, StatusBar, Image } from 'react-native'
+import AppIntroSlider from 'react-native-app-intro-slider';
+import MainNavigation from '../publics/navigations/MainNavigation';
 
+export class Welcome extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+      show_Main_App: <MainNavigation />
+
+    };
+  }
+  on_Done_all_slides = () => {
+    this.setState({ show_Main_App });
+  };
+
+  on_Skip_slides = () => {
+    this.setState({ show_Main_App });
+  }
+
+  render() {
+
+    return (
+      <>
+        <StatusBar translucent backgroundColor="transparent" />
+        <AppIntroSlider
+          slides={slides}
+          onDone={this.on_Done_all_slides}
+          showSkipButton={true}
+          onSkip={this.on_Skip_slides}
+        />
+      </>
+    );
+  }
+}
+const styles = StyleSheet.create({
+
+  MainContainer: {
+    flex: 1,
+    paddingTop: (Platform.OS) === 'ios' ? 20 : 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20
+  },
+  title: {
+    fontSize: 26,
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 20,
+  },
+  text: {
+    color: '#fff',
+    fontSize: 20,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain'
+  }
+});
+
+const slides = [
+  {
+    key: 'k1',
+    title: 'Servis Elektronik',
+    text: 'Butuh teknisi elektronik? kamu bisa cari di sini!',
+    image: require('../assets/images/service-komputer.png'),
+    titleStyle: styles.title,
+    textStyle: styles.text,
+    imageStyle: styles.image,
+    backgroundColor: '#011f4b',
+  },
+  {
+    key: 'k2',
+    title: 'Servis Kendaraan',
+    text: 'Butuh teknisi kendaraan? kamu bisa cari di sini!',
+    image: require('../assets/images/plumber-35611_960_720.png'),
+    titleStyle: styles.title,
+    textStyle: styles.text,
+    imageStyle: styles.image,
+    backgroundColor: '#03396c',
+  },
+  {
+    key: 'k3',
+    title: 'Servis Darurat!',
+    text: 'Semua servis ada dalam satu genggaman!',
+    image: require('../assets/images/1.png'),
+    titleStyle: styles.title,
+    textStyle: styles.text,
+    imageStyle: styles.image,
+    backgroundColor: '#6497b1',
+  }
+];
+export default Welcome
